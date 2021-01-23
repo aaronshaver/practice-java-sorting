18e8f04d7e60d0160bb7212b132f3eb03c392a48
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-01-22 16:14:39 -0800

    boom

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,50 @@
 package com.company;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<Integer> ints = new ArrayList<>();
+        ints.add(4);
+        ints.add(1);
+        ints.add(3);
+        ints.add(2);
+        ints.add(7);
+
+        for (Integer num : ints) {
+            System.out.println(num);
+        }
+        System.out.println();
+
+        Collections.sort(ints);
+
+        for (Integer num : ints) {
+            System.out.println(num);
+        }
+        System.out.println();
+
+        ArrayList<LocalDateTime> localDateTimes = new ArrayList<>();
+        localDateTimes.add(LocalDateTime.parse("2021-01-02T13:11:51.141"));
+        localDateTimes.add(LocalDateTime.parse("2021-01-01T13:11:51.141"));
+        localDateTimes.add(LocalDateTime.parse("2021-01-04T13:11:51.141"));
+        localDateTimes.add(LocalDateTime.parse("2021-01-03T13:11:51.141"));
+        localDateTimes.add(LocalDateTime.parse("2021-01-05T13:11:51.141"));
+
+        for (LocalDateTime localDateTime : localDateTimes) {
+            System.out.println(localDateTime);
+        }
+        System.out.println();
+
+        Collections.sort(localDateTimes);
+
+        for (LocalDateTime localDateTime : localDateTimes) {
+            System.out.println(localDateTime);
+        }
+        System.out.println();
     }
 }
